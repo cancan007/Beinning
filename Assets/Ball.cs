7e803f92e30dbe150ada2e9e946f0d8fa3e64e6a
cancan007 <81799825+cancan007@users.noreply.github.com>
2021-06-25 20:33:17 +0900

    210625
    コードを書いた

---
+++ Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody myRigid;   // Unity上で設定した物理エンジンであるRigidbodyをmyRigidという変数に入れる予定のため、宣言しておく
+    
+    // Start is called before the first frame update
+    void Start()  //初めの処理
+    {
+        myRigid = this.GetComponent<Rigidbody>();  //上で宣言しておいた変数に、実際に代入する(Component)はオブジェクトの内容
+        myRigid.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+        // AddForce: 力を加える, 第一引数でベクトルと力を指定(この場合右斜めに打ち出す), 　ForceMode.Velocityで一回力を加える
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed = 1.0f;  // publicにすることによってUnityに適応後、スクリプト上に変数が表示され調節可能になる
+    // private: このクラス上でしか扱えなくなる
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.transform.position.x > -4)   // 壁より内側なら動くプログラム
+                this.transform.position += Vector3.left * speed * Time.deltaTime;   // thisとはこの場合Playerオブジェクトのこと
+        }    // 上記に関しては 一行のプログラムを実行するときは{}を省いても大丈夫
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.transform.position.x < 4)
+                this.transform.position += Vector3.right * speed * Time.deltaTime;   // ものを動かすときは必ず Time.deltaTimeをつける
+        }
     }
 }
