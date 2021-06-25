be98ac34e018c1dab0d9b6d58136531df77031b4
cancan007 <81799825+cancan007@users.noreply.github.com>
2021-06-25 20:48:04 +0900

    Game1
    Complete

---
+++ Assets/Block.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)    // 衝突時に処理(引数のcollision部分で何に衝突したときか指定できる（今回はすべてなので不要）)
+    {
+        Destroy(this.gameObject);  //このオブジェクトを消す
+    }
+}
